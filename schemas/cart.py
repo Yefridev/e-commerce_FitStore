03d03d077004e890edae95a72089487e7998bd43
Yefri <y.arisa@utp.edu.co>
2026-04-28 15:26:32 -0500

    feat: Crear schemas de carrito

--- schemas/cart.py
+++ schemas/cart.py
@@ -1,11 +1,28 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
 
-class ItemCarritoCrear(BaseModel):
+class CarritoItemAgregar(BaseModel):
     producto_id: int
-    cantidad: int = Field(gt=0)
+    cantidad: int = Field(default=1,gt=1)
 
 
-class ItemCarritoRespuesta(BaseModel):
+class CarritoItemActualizar(BaseModel):
+    cantidad: int = Field(ge=1)
+
+class CarritoItemRespuesta(BaseModel):
+    id: int
     producto_id: int
     cantidad: int
+    nombre: str
+    precio: float
+    subtotal: float
+
+    class Config:
+        from_attributes = True
+
+class CarritoRespuesta(BaseModel):
+    id: int
+    usuario_id: int
+    items: list[CarritoItemRespuesta] = []
+    total: float = 0.0
