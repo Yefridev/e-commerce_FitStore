64771821bed558ee423f3c958859c3bba0ad3595
Yefri <y.arisa@utp.edu.co>
2026-05-02 14:16:25 -0500

    corregir llamado a la tabla usuarios

--- models/address.py
+++ models/address.py
@@ -5,7 +5,7 @@ class Direccion(SQLModel, table=True):
     __tablename__= "direcciones"
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    usuario_id: int = Field(foreign_key="usuario.id")
+    usuario_id: int = Field(foreign_key="usuarios.id")
     calle: str = Field (max_length=200)
     ciudad: str = Field(max_length=100)
     departamento: str = Field(max_length=100)
